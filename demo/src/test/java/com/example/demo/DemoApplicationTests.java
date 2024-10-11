a8dd1212ab04d98d50a1f5daabfa9b4298d5d421
Dmitry Mustakaev <92312298+DmitryMustk@users.noreply.github.com>
2024-10-11 14:55:55 +0700

    Suc collection cicd (#1)
    * - added Dockerfile for app, and add it to docker-compose
    
    * - add jacoco task to gradle for coverage test
    - create ci pipeline (build, test, coverage)
    MIN COVERAGE: 70%
    
    * - create dev version of docker-compose file

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	void mainMethodTest() {
+		assertDoesNotThrow(() -> DemoApplication.main(new String[] {}));
 	}
 
 }
